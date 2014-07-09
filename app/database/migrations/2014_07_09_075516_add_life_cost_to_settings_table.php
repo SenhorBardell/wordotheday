f2a753453467085d87d627030fc500027445f566
John <senhorbardell@gmail.com>
2014-07-09 11:04:15 +0300

    Added life cost migration

---
+++ app/database/migrations/2014_07_09_075516_add_life_cost_to_settings_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class AddLifeCostToSettingsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('settings', function(Blueprint $table)
+		{
+			$table->integer('life_cost');
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('settings', function(Blueprint $table)
+		{
+			$table->dropColumn('life_cost');
+		});
+	}
+
+}
