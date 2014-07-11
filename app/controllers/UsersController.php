dd18ed22fff3ff26bd8e23f400b58514449082ad
John <senhorbardell@gmail.com>
2014-07-11 13:27:55 +0300

    Debug

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -107,31 +107,29 @@ class UsersController extends ApiController {
 
 			return $oldUser;
 
+		} else {
+			$user = User::create(array(
+				'username' => Input::get('username'),
+				'password' => Hash::make(Input::get('username')),
+				'word_id' => SentWordCard::orderBy('created_at', 'desc')->first()->word_id,
+				// 'balance' => Input::has('balance') ? Input::get('balance') : 0,
+				// 'overal_standing' => 0,
+				// 'max_result' => 0,
+			));
+
+			if ($user)
+				return $this->respond(array(
+					'username' => $user['username'],
+					'max_result' => $user['max_result'],
+					'overal_standing' => $user['overal_standing'],
+					'balance' => $user['balance'],
+					'id' => $user['id'],
+					'password' => $user['password'],
+					'word_id' => $user['word_id']
+				));
+
+			return $this->respondServerError('Error creating user');
 		}
-		
-		// } else {
-		// 	$user = User::create(array(
-		// 		'username' => Input::get('username'),
-		// 		'password' => Hash::make(Input::get('username')),
-		// 		'word_id' => SentWordCard::orderBy('created_at', 'desc')->first()->word_id,
-		// 		// 'balance' => Input::has('balance') ? Input::get('balance') : 0,
-		// 		// 'overal_standing' => 0,
-		// 		// 'max_result' => 0,
-		// 	));
-
-		// 	if ($user)
-		// 		return $this->respond(array(
-		// 			'username' => $user['username'],
-		// 			'max_result' => $user['max_result'],
-		// 			'overal_standing' => $user['overal_standing'],
-		// 			'balance' => $user['balance'],
-		// 			'id' => $user['id'],
-		// 			'password' => $user['password'],
-		// 			'word_id' => $user['word_id']
-		// 		));
-
-		// 	return $this->respondServerError('Error creating user');
-		// }
 		
 	}
 
