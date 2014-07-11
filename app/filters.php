952d69926c1f1c30939c0d5611f4c3d698ab87cd
John <senhorbardell@gmail.com>
2014-07-11 12:53:56 +0300

    Debug

--- app/filters.php
+++ app/filters.php
@@ -35,10 +35,10 @@ App::after(function($request, $response)
 
 Route::filter('auth', function()
 {
-	// if (Input::get('auth') != '123')
-		// return Response::json(array('error' => array('message' => 'unauthorized', 'status_code' => 403)));
-	$user = User::where('password', Input::get('auth'))->first();
-	if ($user->isEmpty) return Response::json(array('status' => 'unauthorized'));
+	if (Input::get('auth') != '123')
+		return Response::json(array('error' => array('message' => 'unauthorized', 'status_code' => 403)));
+	// $user = User::where('password', Input::get('auth'))->first();
+	// if ($user->isEmpty) return Response::json(array('status' => 'unauthorized'));
 });
 
 
