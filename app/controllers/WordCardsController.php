d7b352a4bd821606791269a2d0ec21d180932324
John <senhorbardell@gmail.com>
2014-09-04 19:30:06 +0300

    SentWords fix 2

--- app/controllers/WordCardsController.php
+++ app/controllers/WordCardsController.php
@@ -37,9 +37,7 @@ class WordCardsController extends ApiController {
 		$user = User::find(Input::get('user_id'));
 
 		$words = WordCard::take(19)->get()->toArray();
-		$word = WordCard::find(Input::get('id_last_word'));
-		if ($word)
-			$word = $word->toArray();
+		$word = Setting::first()->toArray();
 
 		array_push($words, $word);
 
