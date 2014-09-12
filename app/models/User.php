3f772b2348f51b9d50cdf86908c8d3c3c750edfa
John <senhorbardell@gmail.com>
2014-09-12 17:47:57 +0300

    Pre merge commit

--- app/models/User.php
+++ app/models/User.php
@@ -5,7 +5,7 @@ class User extends \Eloquent {
 	protected $fillable = ['username', 'password', 'word_id', 'balance', 'device'];
 
 	public function subscriptions() {
-		return $this->hasMany('Subscription');
+        return $this->belongsToMany('Category', 'subscriptions', 'user_id', 'category_id')->withTimestamps();
 	}
 
 	public function devices() {
